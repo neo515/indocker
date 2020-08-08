51aa92cf727680eb2376f7ff64291eb825e4579f
jiafeicat <719641500@qq.com>
2020-08-08 16:10:01 +0800

    重调mysql配置文件的映射方式.

--- run/config.sh
+++ run/config.sh
@@ -5,6 +5,11 @@ svc_path=/Volumes/Data/service    #修改成你准备放置数据库服务的根
 
 ###################################################
 
+# echo $0
 project_path=$(dirname $(cd `dirname $0`;pwd))
 
 cfg_tmpl_path=${project_path}/conf_tmpl
+
+# echo "project_path:  $project_path"
+# echo "cfg_tmpl_path: $cfg_tmpl_path"
+# echo
--- run/creat_mysql.sh
+++ run/creat_mysql.sh
@@ -4,13 +4,16 @@ source config.sh
 read -p '请输入你要使用的端口: ' port
 
 service_name=mysql
-# mongodb_main_path=${svc_path}/mongodb-${port}
-service_path=${svc_path}/${service_name}-${port}
-service_data=${service_path}/data
-service_etc=${service_path}/etc
+# svc_path=/Data/service  #config.sh
 
-mkdir -p ${service_path}
-cp -r ${cfg_tmpl_path}/${service_name} $service_etc
+service_path=${svc_path}/${service_name}-${port}  # eg: /Data/service/mysql-3307
+mkdir -p ${service_path}   # eg: /Data/service/mysql-3307
+
+service_data=${service_path}/data  # eg: /Data/service/mysql-3307/data
+service_etc=${service_path}/etc    # eg: /Data/service/mysql-3307/etc
+
+cp -r ${cfg_tmpl_path}/${service_name}/ $service_etc
+mkdir -p  $service_etc/conf.d/
 
 # 新建立时会自动进行初始化，不要建立datadir目录(mysql1)， 让docker自己生成；
 # 否则识别为已存在的db,不会自动初始化
@@ -21,9 +24,13 @@ docker run  --restart=always \
     -h ${service_name}-${port} \
     --memory 4G \
     -v ${service_data}:/var/lib/mysql \
-    -v ${service_etc}:/etc/mysql/conf.d  \
+    -v ${service_etc}:/etc/mysql \
+    -v /etc/localtime:/etc/localtime  \
     -e MYSQL_ROOT_PASSWORD=root \
     -p ${port}:3306 \
     -l creator=indocker \
     -tid mysql:5.6.39
-    # -v /etc/localtime:/etc/localtime \
+
+    # -v ${service_etc}/my.cnf:/etc/mysql/my.cnf \
+    # -v ${service_etc}/conf.d/:/etc/mysql/conf.d \
+
